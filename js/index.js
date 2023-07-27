361cab7cdb2ab203ee5c21e40b2867959f65664a
Ivan Rocha <ivan.rocha.0987@gmail.com>
2023-07-27 20:16:17 -0300

    Commit

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
-const avatar = document.querySelector('[data-avatar]')
+const avatar = document.querySelector('avatar')
+const id = document.getElementById('id')
+const user = document.getElementById('user')
+const createdAccount = document.getElementBy('account')
+const searchId = document.getElementsByName('searchId')
 
+fetch('')

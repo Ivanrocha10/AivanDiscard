6489d15d6dfe2e0ae0e2916b6c801df217e9b10e
Ivan Rocha <ivan.rocha.0987@gmail.com>
2023-07-28 19:27:09 -0300

    Commit

--- js/index.js
+++ js/index.js
@@ -6,26 +6,29 @@ const createdAccount = document.getElementById('account')
 const searchFrom = document.forms['user-code']
 const searchInput = searchFrom.querySelector('input')
 
-searchFrom.addEventListener('submit', (e) => {
+searchFrom.addEventListener('submit', e => {
   e.preventDefault()
   const searchValue = searchInput.value
-  
+
   if (searchValue === '') {
     alert('Por favor, digite um Id do Discord!')
     return
-  } 
+  }
   fetch(`https://discordlookup.mesavirep.xyz/v1/user/${searchValue}`)
-
-    .then((response) => response.json())
-    .then((data) => {
-      const formattedDate = new Date(data.created_at).toLocaleDateString('pt-BR', {
-        day: 'numeric',
-        month: 'numeric',
-        year: 'numeric',
-      })
+    .then(response => response.json())
+    .then(data => {
+      const formattedDate = new Date(data.created_at).toLocaleDateString(
+        'pt-BR',
+        {
+          day: 'numeric',
+          month: 'numeric',
+          year: 'numeric'
+        }
+      )
       if (data.banner.link) {
         banner.style.backgroundImage = `url(${data.banner.link})`
       } else {
+        banner.style.backgroundImage = 'none'
         banner.style.backgroundColor = data.banner.color
       }
 
@@ -34,7 +37,7 @@ searchFrom.addEventListener('submit', (e) => {
       user.innerText = data.tag
       createdAccount.innerText = formattedDate
     })
-    .catch((error) => {
+    .catch(error => {
       console.log(error)
     })
 })

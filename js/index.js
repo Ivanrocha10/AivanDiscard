b8c9664d051a67a50f93a6620187a607d55600e4
Ivan Rocha <ivan.rocha.0987@gmail.com>
2023-07-27 21:49:26 -0300

    Commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const avatar = document.getElementById('avatar')
 const id = document.getElementById('id')
+const banner = document.getElementById('banner')
 const user = document.getElementById('user')
 const createdAccount = document.getElementById('account')
 const searchFrom = document.forms['user-code']
@@ -8,19 +9,28 @@ const searchInput = searchFrom.querySelector('input')
 searchFrom.addEventListener('submit', (e) => {
   e.preventDefault()
   const searchValue = searchInput.value
+  
   if (searchValue === '') {
     alert('Por favor, digite um Id do Discord!')
     return
-  }
+  } 
   fetch(`https://discordlookup.mesavirep.xyz/v1/user/${searchValue}`)
-    .then(response => response.json())
-    .then(data => {
-      avatar.src = data.avatar
+    .then((response) => response.json())
+    .then((data) => {
+      
+      if (data.banner.link) {
+        banner.style.background = `url(${data.banner.link})`
+      } else {
+        banner.style.background = data.banner.color
+      }
+ 
+
+      avatar.src = data.avatar.link
       id.innerText = data.id
-      user.innerText = data.username
+      user.innerText = data.tag
       createdAccount.innerText = data.created_at
     })
-    .catch(error => {
+    .catch((error) => {
       console.log(error)
     })
 })

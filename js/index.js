0a9b0be91bf8abedfd4efaf84152e4907c902f10
Ivan Rocha <ivan.rocha.0987@gmail.com>
2023-07-27 21:07:36 -0300

    Commit

--- js/index.js
+++ js/index.js
@@ -2,6 +2,22 @@ const avatar = document.querySelector('avatar')
 const id = document.getElementById('id')
 const user = document.getElementById('user')
 const createdAccount = document.getElementBy('account')
-const searchId = document.getElementsByName('searchId')
+const searchFrom = document.forms['user-code']
+const searchInput = searchFrom.querySelector('input')
 
-fetch('')
+searchFrom.addEventListener('submit', e => {
+  e.preventDefault()
+  const searchValue = searchInput.value
+
+  fetch(`https://discordlookup.mesavirep.xyz/v1/user/${searchId}`)
+    .then(response => response.json())
+    .then(data => {
+      avatar.src = data.avatar
+      id.innerText = data.id
+      user.innerText = data.username
+      createdAccount.innerText = data.created_at
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})

029a53b0256261b16a5cd957de6f65916e708c6b
Ivan Rocha <ivan.rocha.0987@gmail.com>
2023-07-28 21:00:33 -0300

    Commit

--- js/index.js
+++ js/index.js
@@ -1,77 +1,56 @@
 const avatar = document.getElementById('avatar')
 const id = document.getElementById('id')
-const banner = document.getElementById('banner')
 const user = document.getElementById('user')
+const banner = document.getElementById('banner')
 const createdAccount = document.getElementById('account')
-const searchFrom = document.forms['user-code']
-const searchInput = searchFrom.querySelector('input')
+const searchForm = document.forms['user-code']
+const searchInput = searchForm.querySelector('input')
 const badge = document.querySelector('.badge img')
 
 const badgeUrls = {
-  DISCORD_EMPLOYEE: 
-    './assets/badges/discord_employee.svg',
-  PARTNERED_SERVER_OWNER: 
-    './assets/badges/partnered_server_owner.svg',
-  HYPESQUAD_EVENTS: 
-    './assets/badges/hypesquad_events.svg',
-  BUGHUNTER_LEVEL_1: 
-    './assets/badges/bughunterlevel1.svg',
-  HOUSE_BRAVERY: 
-    './assets/badges/hypesquadbravery.svg',
-  HOUSE_BRILLIANCE: 
-    './assets/badges/hypesquadbrilliance.svg',
-  HOUSE_BALANCE: 
-    './assets/badges/hypesquadbalance.svg',
-  EARLY_SUPPORTER: 
-    './assets/badges/earlysupporter.svg',
-  TEAM_USER: 
-    './assets/badges/team_user.svg',
-  BUGHUNTER_LEVEL_2: 
-    './assets/badges/bughunter_level_2.svg',
-  VERIFIED_BOT: 
-    './assets/badges/verified_bot.svg',
+  DISCORD_EMPLOYEE: './assets/badges/discord_employee.svg',
+  PARTNERED_SERVER_OWNER: './assets/badges/partnered_server_owner.svg',
+  HYPESQUAD_EVENTS: './assets/badges/hypesquad_events.svg',
+  BUGHUNTER_LEVEL_1: './assets/badges/bughunterlevel1.svg',
+  HOUSE_BRAVERY: './assets/badges/hypesquadbravery.svg',
+  HOUSE_BRILLIANCE: './assets/badges/hypesquadbrilliance.svg',
+  HOUSE_BALANCE: './assets/badges/hypesquadbalance.svg',
+  EARLY_SUPPORTER: './assets/badges/earlysupporter.svg',
+  TEAM_USER: './assets/badges/team_user.svg',
+  BUGHUNTER_LEVEL_2: './assets/badges/bughunter_level_2.svg',
+  VERIFIED_BOT: './assets/badges/verified_bot.svg',
   EARLY_VERIFIED_BOT_DEVELOPER:
     './assets/badges/early_verified_bot_developer.svg',
   DISCORD_CERTIFIED_MODERATOR:
     './assets/badges/discord_certified_moderator.svg',
-  BOT_HTTP_INTERACTIONS: 
-    './assets/badges/bot_http_interactions.svg',
-  SPAMMER: 
-    './assets/badges/spammer.svg',
-  ACTIVE_DEVELOPER: 
-    './assets/badges/activedeveloper.svg',
-  QUARANTINED: 
-    './assets/badges/quarantined.svg',
-  GATEWAY_PRESENCE: 
-    './assets/badges/gateway_presence.svg',
-  GATEWAY_PRESENCE_LIMITED: 
-    './assets/badges/gateway_presence_limited.svg',
-  GATEWAY_GUILD_MEMBERS: 
-   './assets/badges/gateway_guild_members.svg',
+  BOT_HTTP_INTERACTIONS: './assets/badges/bot_http_interactions.svg',
+  SPAMMER: './assets/badges/spammer.svg',
+  ACTIVE_DEVELOPER: './assets/badges/activedeveloper.svg',
+  QUARANTINED: './assets/badges/quarantined.svg',
+  GATEWAY_PRESENCE: './assets/badges/gateway_presence.svg',
+  GATEWAY_PRESENCE_LIMITED: './assets/badges/gateway_presence_limited.svg',
+  GATEWAY_GUILD_MEMBERS: './assets/badges/gateway_guild_members.svg',
   GATEWAY_GUILD_MEMBERS_LIMITED:
     './assets/badges/gateway_guild_members_limited.svg',
   VERIFICATION_PENDING_GUILD_LIMIT:
     './assets/badges/verification_pending_guild_limit.svg',
-  EMBEDDED: 
-    './assets/badges/embedded.svg',
-  GATEWAY_MESSAGE_CONTENT: 
-    './assets/badges/gateway_message_content.svg',
+  EMBEDDED: './assets/badges/embedded.svg',
+  GATEWAY_MESSAGE_CONTENT: './assets/badges/gateway_message_content.svg',
   GATEWAY_MESSAGE_CONTENT_LIMITED:
     './assets/badges/gateway_message_content_limited.svg',
-  APPLICATION_COMMAND_BADGE: 
-    './assets/badges/application_command_badge.svg'
+  APPLICATION_COMMAND_BADGE: './assets/badges/application_command_badge.svg'
 }
 
 function getBadgeSrc(flag) {
   return badgeUrls[flag] || null
 }
 
-searchFrom.addEventListener('submit', e => {
+searchForm.addEventListener('submit', e => {
   e.preventDefault()
   const searchValue = searchInput.value
 
   if (searchValue === '') {
-    alert('Por favor, digite um Id do Discord!')
+    alert('Por favor, informe um ID do Discord!')
     return
   }
   fetch(`https://discordlookup.mesavirep.xyz/v1/user/${searchValue}`)
@@ -85,6 +64,7 @@ searchFrom.addEventListener('submit', e => {
           year: 'numeric'
         }
       )
+
       if (data.banner.link) {
         banner.style.backgroundImage = `url(${data.banner.link})`
       } else {
@@ -96,7 +76,7 @@ searchFrom.addEventListener('submit', e => {
       id.innerText = data.id
       user.innerText = data.tag
       createdAccount.innerText = formattedDate
-      badge.src = data.badge
+      badge.src = getBadgeSrc(data.badges[0])
     })
     .catch(error => {
       console.log(error)
